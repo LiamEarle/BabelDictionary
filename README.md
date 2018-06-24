9d17d71ce3d402eb484af6e3a0c8b78511217cf9
LiamEarle <liam.wilkinson@gmail.com>
2018-06-24 22:04:22 +1000

    Change dictionary source, optimise search code

--- README.md
+++ README.md
@@ -4,10 +4,15 @@
 **BabelDictionary** is a Python project that analyzes a random page of [The Library of Babel](https://libraryofbabel.info/) and checks for dictionary words. It then posts the three longest words to the Twitter account [@BabelDictionary](https://twitter.com/BabelDictionary)
 
 #### How does it work?
-BabelDictionary requests a random page from the library and uses the *lxml* library to scrape the page title and contents. Then, using a list of 10,000 commonly used English words provided by [Google](https://github.com/first20hours/google-10000-english) it searches the contents for any matches and returns the three longest word results. Simple!
+BabelDictionary requests a random page from the library and uses the *lxml* library to scrape the page title and contents. Then, using a list of 466,000  English words provided by [this repository](https://github.com/dwyl/english-words) it searches the contents for any matches and returns the three longest word results. Simple!
 
 #### Why though?  ¯\\\_(ツ)_/¯
 The Library of Babel has always been an interesting concept to me, and who doesn't love a Twitter Bot? Maybe we'll find something interesting.
+
+#### Changelog
+* **24th June 2018**
+    * Updated dictionary source
+    * Optimise searching method
 
 ##### Attribution
 * [Twitter Profile Logo](https://www.vecteezy.com/vector-art/168469-free-books-libro-icons-vector) 
--- babeldictionary.py
+++ babeldictionary.py
@@ -3,6 +3,7 @@ import re
 import twitter
 import logging
 
+from datetime import datetime
 from credentials import TwitterCredentials
 from lxml import html
 
@@ -18,13 +19,14 @@ def main():
     # API Setup (Twitter)
     logging.info('Initializing Twitter API')
     credentials = TwitterCredentials('./twitter_secrets.json')
-    twitter_api = twitter.Api(consumer_key=credentials.get_consumer_key(),
-                              consumer_secret=credentials.get_consumer_secret(),
-                              access_token_key=credentials.get_access_token_key(),
-                              access_token_secret=credentials.get_access_token_secret())
+    TwitterAPI = twitter.Api(consumer_key=credentials.get_consumer_key(),
+                             consumer_secret=credentials.get_consumer_secret(),
+                             access_token_key=credentials.get_access_token_key(),
+                             access_token_secret=credentials.get_access_token_secret())
 
     # Parse Dictionary
-    dictionary = open('./dictionary/google-10000-english.txt').read().split('\n')
+    with open('./dictionary/words_alpha.txt') as file:
+        dictionary = set(file.read().split('\n'))
 
     # Request Library of Babel Page & Parse
     logging.info('Requesting Library of Babel Page')
@@ -45,28 +47,35 @@ def main():
     found_words = []
 
     logging.info('Searching...')
+    search_start = datetime.now()
     for word in dictionary:
-        match = re.search(word, babel['text'])
+        if word in babel['text']:
+            found_words.append(word)
 
-        if match:
-            found_words.append(match.group(0))
-
+    search_time = (datetime.now() - search_start).microseconds
     if len(found_words) > 0:
         longest_words = sorted(found_words, key=len)[-1:-4:-1]  # Longest 3 Words
+        logging.info('Dictionary search took {}ms...'.format(search_time))
         logging.info('Matches: {matches} - Longest Words: {words}'.format(matches=len(found_words), words=longest_words))
     else:
         logging.warning('No matches were found, did something go wrong?')
 
     # Post a Tweet
     hashtags = ['#libraryofbabel', '#babeldictionary']
-    tweet = "\U0001f4da {title} \n\n Total Words: {total_words}\n \U0001f947 {first}\n \U0001f948 " \
-            "{second}\n \U0001f949 {third} \n\n \U0001f517 {url} {hashtags}" \
+    tweet = "\U0001f4c3 {title}\n\n" \
+            "Total Words (3 letters or longer): {total_words}\n" \
+            "\U0001f947 {first_word}\n" \
+            "\U0001f948 {second_word}\n" \
+            "\U0001f949 {third_word}\n\n" \
+            "\U0001f517 {url} {hashtags}" \
         .format(title=babel['title'], total_words=len(list(filter(lambda x: len(x) >= 3, found_words))),
-                first=longest_words[0], second=longest_words[1], third=longest_words[2], url=request.url,
+                first_word=longest_words[0], second_word=longest_words[1], third_word=longest_words[2], url=request.url,
                 hashtags=' '.join(hashtags))
 
+    print(tweet)
+
     logging.info('Posting to Twitter')
-    twitter_api.PostUpdate(status=tweet)
+    #TwitterAPI.PostUpdate(status=tweet)
 
 
 if __name__ == '__main__':
